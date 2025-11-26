d7bd2081fcb53f4c5d61d5c150e6d57a903ec68f
Suhan J Bangera <s.bangera@castsoftware.com>
2025-11-26 13:25:50 +0530

    Added a module system

--- pkg/module/module.go
+++ pkg/module/module.go
@@ -5,6 +5,5 @@ import "github.com/spyder01/lilium-go/pkg/core"
 type Module interface {
 	Init(app *core.Lilium) error
 	Start(app *core.Lilium) error
-	Stop(app *core.Lilium) error
-	Close(app *core.Lilium) error
+	Shutdown(app *core.Lilium) error
 }
